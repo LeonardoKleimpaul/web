168bb45bef40110b6b1f5049fcf83b8b15998cea
LeonardoKleimpaul <leonardokleimpaul@protonmail.com>
2023-02-09 22:30:56 -0300

    Spring data

---
+++ aula50/exemplo_H2/src/main/java/br/edu/unoesc/exemplo_H2/model/Livro.java
@@ -0,0 +1,14 @@
+package br.edu.unoesc.exemplo_H2.model;
+
+import org.springframework.data.annotation.Id;
+
+public record Livro(
+		
+		@Id
+		Integer id,
+		String titulo,
+		Integer paginas,
+		String autor
+				
+){
+}
