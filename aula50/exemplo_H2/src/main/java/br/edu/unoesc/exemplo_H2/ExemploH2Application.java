4b1e34a762e9174ad5be73539b27f939494c85e0
LeonardoKleimpaul <leonardokleimpaul@protonmail.com>
2023-02-10 22:26:38 -0300

    Spring data com JPQL

--- aula50/exemplo_H2/src/main/java/br/edu/unoesc/exemplo_H2/ExemploH2Application.java
+++ aula50/exemplo_H2/src/main/java/br/edu/unoesc/exemplo_H2/ExemploH2Application.java
@@ -1,5 +1,7 @@
 package br.edu.unoesc.exemplo_H2;
 
+import java.util.Optional;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,9 +21,37 @@ public class ExemploH2Application {
 	CommandLineRunner commandLineRunner(LivroRepository repositorio) {
 		return args -> {
 			Livro l = new Livro(null, "O senhor dos pastéis", 666, "Tolkien");
-			repositorio.save(l);
+			l = repositorio.save(l);
+			System.out.println(l);
+			
+			repositorio.save(new Livro(null, "O Hobbit", 42, "Tolkien"));
+			
+			Optional<Livro> p = repositorio.findById(1);
+			if(p.isPresent()) {
+				System.out.println(p.get());				
+			} else {
+				System.out.println("Registro nao enontrado");
+			}
+			
+//			l = repositorio.findById(3).get();
+//			l.setTitulo("Título nada a ver");
+//			l.setPaginas(100);
+//			l.setAutor("Fulando da Silva de Sauro");
+//			repositorio.save(l);
 			
 			System.out.println(repositorio.findAll());
+			
+			for(var livro: repositorio.findByAutorContainingIgnoreCase("hec")) {
+				System.out.println(livro);
+			}
+			
+			for(var livro: repositorio.porQtdPaginas(300)) {
+				System.out.println(livro);
+			}
+			
+			for(var livro: repositorio.findByFiltro("senhor")) {
+				System.out.println(livro);
+			}
 		};
 	}
 }
--- aula50/exemplo_H2/src/main/java/br/edu/unoesc/exemplo_H2/model/Livro.java
+++ aula50/exemplo_H2/src/main/java/br/edu/unoesc/exemplo_H2/model/Livro.java
@@ -1,14 +1,28 @@
 package br.edu.unoesc.exemplo_H2.model;
 
-import org.springframework.data.annotation.Id;
+import java.io.Serializable;
 
-public record Livro(
-		
-		@Id
-		Integer id,
-		String titulo,
-		Integer paginas,
-		String autor
-				
-){
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Livro implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Integer id;
+	
+	String titulo;
+	Integer paginas;
+	String autor;
+
 }
